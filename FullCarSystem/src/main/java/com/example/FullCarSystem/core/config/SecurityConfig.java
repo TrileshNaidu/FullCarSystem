4907ce7dae3190bdf6680722f608f5c3c56576ae
TrileshNaidu <trileshnaidukatreddy@gmail.com>
2025-07-16 10:59:38 +0530

    user module complted

--- FullCarSystem/src/main/java/com/example/FullCarSystem/core/config/SecurityConfig.java
+++ FullCarSystem/src/main/java/com/example/FullCarSystem/core/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults()); // Enable Basic Auth for testing
